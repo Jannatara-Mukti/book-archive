c93a2edf5ec6e315b9f4e3ae4573ca6f56c57858
Jannatara Mukti <jannatara.mukti@email.com>
2021-09-05 22:08:07 +0600

    first commit

---
+++ js/api.js
@@ -0,0 +1,9 @@
+const searchBook = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
